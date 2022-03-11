608c2543ca6cefa3b9a9049a8a85c2f017082894
joaogabriels <joaogabrielsevangelista@gmail.com>
2022-03-11 19:37:02 -0300

    Remoção de console.log

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,7 +9,6 @@ import { Container } from "./styles";
 export function Summary() {
     const { transactions } = useTransactions();
     
-    console.log(transactions);
 
     const summary = transactions.reduce((acc, transaction) => { 
         if (transaction.type === 'deposit'){
